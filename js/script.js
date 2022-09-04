dd29e3a911a8886261caa0b37086276c96ce7390
Sasha <exemple@gmail.com>
2022-09-04 17:51:22 +0300

    First app

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-const vehicleBodyWidth = 5000;
-const vehicleBodyLength = 4000;
-
-console.log("Ширина кузова автомобиля: " + vehicleBodyWidth +', длина: ' + vehicleBodyLength);
+const numberOfFilms = +prompt('Сколько фильмов от пользователя вы уже посмотрели?','');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один из последних просмотренных фильмов?','');
+const b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+    
+      personaMovieDB.movis[a] = b;    
+      personaMovieDB.movis[c] = d;  
+      console.log(personaMovieDB);
+      
